263c7eb7335c6f9eabd159987c4e114eb684463e
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-29 19:24:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,5 +83,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidayAry = []
+  holiday_hash.each do |season, holidays|
+    if holidays.include?("BBQ")
+      holidayAry << holidays
+    end
+  end
+  return holidayArt
 end
